00a9e72d03249284a9f537f7b08874cbcbad5bf8
Xenorio <realxenorio@gmail.com>
2022-01-22 23:45:32 +0100

    Administrative commands

---
+++ commands/add.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const config = require('../config.js')
+const fetch = require('cross-fetch')
+
+module.exports.run = async(client, interaction) => {
+
+    if (interaction.guildId != config.devGuild) {
+        interaction.reply({ content: 'This guild is not allowed to use administrative commands!', ephemeral: true })
+        return
+    }
+
+    if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+        interaction.reply({ content: "Only administrators are allowed to use this!", ephemeral: true })
+        return
+    }
+
+    let domain = interaction.options.get('domain', true).value
+
+    let res = await fetch(config.api + '/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': config.apiKey
+        },
+        body: JSON.stringify({
+            domain: domain
+        })
+    })
+
+    interaction.reply({ content: `Added **${domain}** to blocklist`, ephemeral: true })
+
+}
+
+module.exports.devGuildOnly = true
+
+module.exports.builder = new SlashCommandBuilder()
+    .setName('add')
+    .setDescription('Adds a domain to the blocklist')
+    .addStringOption(option =>
+        option.setName('domain')
+        .setDescription('The domain to add')
+        .setRequired(true)
+    )
